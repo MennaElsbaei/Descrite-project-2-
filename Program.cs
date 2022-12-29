056db3de51ad2a6bbef558047e95255a4ae363cc
MennaElsbaei <116191134+MennaElsbaei@users.noreply.github.com>
2022-12-29 16:56:54 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Security;
+
+namespace perfect_number__project_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the start : ");
+            int start = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Please enter the end : ");
+            int end = int.Parse(Console.ReadLine());
+
+
+
+            for (int i = start; i <= end; i++)
+            {
+                int sum = 0;
+
+                for (int j = 1; j < i; j++)
+                {
+
+
+                    if (i % j == 0)
+                    {
+                        sum += j;
+                    }
+                }
+                if (sum == i)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+
+        }
+    }
+}
